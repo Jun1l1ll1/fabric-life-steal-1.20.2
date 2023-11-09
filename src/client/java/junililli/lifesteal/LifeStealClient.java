22996f797fe2d225ff9be5f51beb636a69ef4a4a
Jun1l1ll1 <jujoa007@osloskolen.no>
2023-11-09 18:33:06 +0100

    initial commit

---
+++ src/client/java/junililli/lifesteal/LifeStealClient.java
@@ -0,0 +1,10 @@
+package junililli.lifesteal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LifeStealClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
